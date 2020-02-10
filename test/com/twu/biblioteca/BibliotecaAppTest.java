fa6c0eef2c5c07a8873b841b0bf1a9d4c07c2e95
alwinao <alwina.o@thoughtworks.com>
2020-02-10 17:29:21 -0600

    add test to check in book

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -16,5 +16,18 @@ public class BibliotecaAppTest {
         assertEquals(4, app.getAvailableBooks().size());
         app.checkoutBook("Gone With The Wind");
         assertEquals(3, app.getAvailableBooks().size());
+        assertEquals(1, app.checkedOutItems.size());
+    }
+
+    @Test
+    public void shouldCheckInBook() {
+        BibliotecaApp app = new BibliotecaApp();
+
+        app.populateBooks();
+
+        app.checkoutBook("Gone With The Wind");
+        assertEquals(1, app.checkedOutItems.size());
+        app.checkinBook("Gone With The Wind");
+        assertEquals(0, app.checkedOutItems.size());
     }
 }
