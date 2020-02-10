be963a67d80de37570dfc52f32e3025110a9cee2
alwinao <alwina.o@thoughtworks.com>
2020-02-09 22:03:43 -0600

    2.2 - update app to checkin/checkout movie

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -8,6 +8,8 @@ import static java.lang.System.exit;
 public class BibliotecaApp {
 
     ArrayList<Book> books = new ArrayList<Book>();
+    ArrayList<Movie> movies = new ArrayList<Movie>();
+
 
     public static void main(String[] args) {
 
@@ -18,14 +20,12 @@ public class BibliotecaApp {
         BibliotecaApp app = new BibliotecaApp();
 
         app.populateBooks();
+        app.populateMovies();
 
 //        list out menu options
         System.out.println("Menu: ");
 
-        System.out.println("1 - List of Books");
-        System.out.println("2 - Check out a book");
-        System.out.println("3 - Check in a book");
-        System.out.println("0 - Exit");
+        app.displayMenu();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -33,7 +33,7 @@ public class BibliotecaApp {
             System.out.println("Please select an option");
             int option = scanner.nextInt();  // Read user input
             System.out.println("Option: " + option);
-            switch(option) {
+            switch (option) {
                 case 1:
                     app.displayBooks();
                     break;
@@ -44,6 +44,17 @@ public class BibliotecaApp {
                 case 3:
                     String returnBookName = app.enterTitle();
                     app.checkinBook(returnBookName);
+                    break;
+                case 4:
+                    app.displayMovies();
+                    break;
+                case 5:
+                    String movieName = app.enterTitle();
+                    app.checkoutMovie(movieName);
+                    break;
+                case 6:
+                    String returnMovieName = app.enterTitle();
+                    app.checkinMovie(returnMovieName);
                     break;
                 case 0:
                     exit(0);
@@ -65,7 +76,7 @@ public class BibliotecaApp {
         ArrayList<Book> availableBooks = new ArrayList<Book>();
 
         for (Book book : books) {
-            if(book.isAvailable()) {
+            if (book.isAvailable()) {
                 availableBooks.add(book);
             }
         }
@@ -87,15 +98,15 @@ public class BibliotecaApp {
     }
 
     public String enterTitle() {
-        System.out.println("Please enter name of book to checkin or checkout a book");
+        System.out.println("Please enter name of item to checkin or checkout.");
         Scanner scanner = new Scanner(System.in);
-        String  name = scanner.nextLine();
+        String name = scanner.nextLine();
         return name;
     }
 
     public void checkoutBook(String name) {
         for (Book book : books) {
-            if(book.getName().equalsIgnoreCase(name) && book.isAvailable()){
+            if (book.getName().equalsIgnoreCase(name) && book.isAvailable()) {
                 System.out.println("Checking out: " + book.getName());
                 book.checkout();
                 System.out.println("Thank you! Enjoy the book.");
@@ -107,42 +118,79 @@ public class BibliotecaApp {
 
     public void checkinBook(String name) {
         for (Book book : books) {
-            if(book.getName().equalsIgnoreCase(name) && !book.isAvailable()){
+            if (book.getName().equalsIgnoreCase(name) && !book.isAvailable()) {
                 System.out.println("Checking in: " + book.getName());
                 book.checkin();
-                System.out.println("Thank you! Enjoy the book.");
+                System.out.println("Thank you for returning the book.");
                 return;
             }
         }
         System.out.println("That is not a valid book to return.");
     }
 
-//    public void checkinBook() {
-//        System.out.println("What book would you like to return? ");
-//        Scanner scanner = new Scanner(System.in);
-//        String name = scanner.nextLine();
-//
-//        for (Book book : books) {
-//
-//            if (book.getName().equalsIgnoreCase(name) && !book.isAvailable()) {
-//                System.out.println("Checking in: " + book.getName());
-//                book.checkin();
-//                System.out.println("Thank you for returning the book.");
-//                return;
-//            }
-//            else
-//                System.out.println("Sorry " + book.getName() + " is not available.");
-//        }
-//    }
+    public void displayMovies() {
+        for (Movie movie : getAvailableMovies()) {
+            System.out.println(movie);
+        }
+    }
 
+    public ArrayList<Movie> getAvailableMovies() {
+        ArrayList<Movie> availableMovies = new ArrayList<Movie>();
 
-    public void addBook() {
+        for (Movie movie : movies) {
+            if (movie.isAvailable()) {
+                availableMovies.add(movie);
+            }
+        }
+        return availableMovies;
+    }
 
+    public void populateMovies() {
+        Movie fastAndFurious = new Movie("Fast and Furious", 2001, "Rob Cohen", "9");
+        movies.add(fastAndFurious);
+
+        Movie speed = new Movie("Speed", 1995, "Jan de Bont", "8");
+        movies.add(speed);
+
+        Movie badBoys3 = new Movie("Bad Boys 3", 2020, "Adil El Arbi", "10");
+        movies.add(badBoys3);
+
+        Movie blackPanther = new Movie("Black Panther", 2018, "Ryan Coogler", "9");
+        movies.add(blackPanther);
+    }
+
+    public void checkoutMovie(String name) {
+        for (Movie movie : movies) {
+            if (movie.getName().equalsIgnoreCase(name) && movie.isAvailable()) {
+                System.out.println("Checking out: " + movie.getName());
+                movie.checkout();
+                System.out.println("Thank you! Enjoy the movie.");
+                return;
+            }
+        }
+        System.out.println("Sorry " + name + " is not available.");
+    }
+
+    public void checkinMovie(String name) {
+        for (Movie movie : movies) {
+            if (movie.getName().equalsIgnoreCase(name) && !movie.isAvailable()) {
+                System.out.println("Checking in: " + movie.getName());
+                movie.checkin();
+                System.out.println("Thank you for returning the movie.");
+                return;
+            }
+        }
+        System.out.println("That is not a valid movie to return.");
+    }
+
+    public void displayMenu(){
+        System.out.println("1 - List of Books");
+        System.out.println("2 - Check out a book");
+        System.out.println("3 - Check in a book");
+        System.out.println("4 - List of Movies");
+        System.out.println("5 - Check out a movie");
+        System.out.println("6 - Check in a movie");
+        System.out.println("0 - Exit");
+        System.out.println();
     }
 }
-
-//if(book.getName().equalsIgnoreCase(name) && !book.isAvailable()){
-//        System.out.println("Checking in: " + book.getName());
-//        book.checkin();
-//        System.out.println("Thank you for returning the book.");
-//        }
